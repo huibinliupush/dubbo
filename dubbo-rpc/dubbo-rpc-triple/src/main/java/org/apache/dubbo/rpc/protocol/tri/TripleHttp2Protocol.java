d83826278431d8feb2b7eff4c8d3ab2e9f1b76cd
bin <huibinliu.coder@gmail.com>
2025-07-24 22:40:27 +0800

    支持 triple rest spring mvc

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TripleHttp2Protocol.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TripleHttp2Protocol.java
@@ -126,13 +126,14 @@ public class TripleHttp2Protocol extends AbstractWireProtocol implements ScopeMo
         String httpVersion = operator.detectResult().getAttribute(TripleProtocolDetector.HTTP_VERSION);
         List<ChannelHandler> channelHandlerPretenders = new ArrayList<>();
         try {
-            // h1
+            // h1 , 可以 upgrade 到 h2c（明文）
             if (HttpVersion.HTTP1.getVersion().equals(httpVersion)) {
                 configurerHttp1Handlers(url, channelHandlerPretenders);
                 return;
             }
 
-            // h2
+            // h2, 直接采用协议嗅探（connection preface），直接探测出 h2 协议（明文）
+            // tls 上通过 ALPN 协商 h2 协议
             if (HttpVersion.HTTP2.getVersion().equals(httpVersion)) {
                 configurerHttp2Handlers(url, channelHandlerPretenders);
             }
@@ -162,7 +163,7 @@ public class TripleHttp2Protocol extends AbstractWireProtocol implements ScopeMo
                 protocol -> {
                     // h2c : tcp 上 http1 升级 http2
                     // http1 使用头字段 Connection:Upgrade 升级到 http2 , 服务端返回状态码 101 切换协议
-                    // h2 : tls 上升级 http2 通过 ALPN, see : org.apache.dubbo.remoting.transport.netty4.NettyPortUnificationServerHandler.enableSsl
+                    // h2 : tls 上 http1 升级 http2 通过 ALPN, see : org.apache.dubbo.remoting.transport.netty4.NettyPortUnificationServerHandler.enableSsl
                     if (AsciiString.contentEquals(Http2CodecUtil.HTTP_UPGRADE_PROTOCOL_NAME, protocol)) {
                         return new Http2ServerUpgradeCodec(
                                 buildHttp2FrameCodec(tripleConfig),
